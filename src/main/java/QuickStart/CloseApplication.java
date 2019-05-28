be4f8c91c938bf4708e35bf48aa48df51cc45a11
Md Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2019-05-28 13:44:27 +0600

    Add files via upload

---
+++ src/main/java/QuickStart/CloseApplication.java
@@ -0,0 +1,14 @@
+package QuickStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class CloseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloseApplication.class, args);
+	}
+
+}
